4a009a2898030d02d41bad3ed92128ebbf1544a0
Anderson Marques <anderson.marques-ext@ab-inbev.com>
2024-11-26 18:53:54 -0300

    Add double linked list

--- data-structures/03.1-double-linked-list/double-linked-list.go
+++ data-structures/03.1-double-linked-list/double-linked-list.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 type MyDoubleLinkedList struct {
 	head   *Node
 	tail   *Node
@@ -48,15 +50,78 @@ func (l *MyDoubleLinkedList) Prepend(value interface{}) {
 		prev:  nil,
 	}
 
-	if l.head == nil {
-		l.head = &newNode
+	newNode.next = l.head
+	l.head.prev = &newNode
+	l.head = &newNode
+	if l.tail == nil {
 		l.tail = &newNode
-	} else {
-		newNode.next = l.head
-		l.head.prev = &newNode
-		l.tail = l.head
-		l.head = &newNode
 	}
 
 	l.length++
+}
+
+func (l *MyDoubleLinkedList) Insert(index int, value interface{}) {
+	if index > 0 && index <= l.length {
+		newNode := Node{
+			value: value,
+			prev:  nil,
+			next:  nil,
+		}
+
+		currentPosition := 1
+		currentNode := l.head
+
+		if currentPosition == index && currentNode.next == nil {
+			l.Append(value)
+		} else {
+			for currentPosition != index && currentNode.next != nil {
+				currentNode = currentNode.next
+				currentPosition++
+			}
+
+			if currentNode.next == nil {
+				l.tail = &newNode
+			}
+			newNode.next = currentNode.next
+			currentNode.next.prev = &newNode
+			newNode.prev = currentNode
+			currentNode.next = &newNode
+			l.length++
+		}
+	}
+}
+
+func (l *MyDoubleLinkedList) Print() {
+	currentNode := l.head
+	for currentNode != nil {
+		fmt.Print("| ", currentNode.value, " ")
+		currentNode = currentNode.next
+	}
+	fmt.Println("")
+}
+
+func (l *MyDoubleLinkedList) PrintReverse() {
+	currentNode := l.tail
+	for currentNode != nil {
+		fmt.Print("| ", currentNode.value, " ")
+		currentNode = currentNode.prev
+	}
+	fmt.Println("")
+}
+
+func main() {
+	myDoubleLinkedList := NewDoubleLinkedList()
+	myDoubleLinkedList.Append(1)
+	myDoubleLinkedList.Append(2)
+	myDoubleLinkedList.Append(3)
+	myDoubleLinkedList.Append(4)
+	myDoubleLinkedList.Insert(2, 5)
+	myDoubleLinkedList.Print()        // 1 2 5 3 4
+	myDoubleLinkedList.PrintReverse() // 4 3 5 2 1
+	myDoubleLinkedList.Insert(1, 6)
+	myDoubleLinkedList.Print()        // 1 6 2 5 3 4
+	myDoubleLinkedList.PrintReverse() // 4 3 5 2 6 1
+	myDoubleLinkedList.Prepend(7)
+	myDoubleLinkedList.Print()        // 7 1 6 2 5 3 4
+	myDoubleLinkedList.PrintReverse() // 4 3 5 2 6 1 7
 }
