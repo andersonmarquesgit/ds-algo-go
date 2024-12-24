ced6b9591fec1dea6271a2ebff4c4d8139a14eb4
Anderson Marques <anderson.marques-ext@ab-inbev.com>
2024-12-23 23:55:14 -0300

    Add sorting insertion sort

---
+++ algorithms/07-sorting-insertion-sort/sorting.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func insertionSort(nums []int) []int {
+	for i := 1; i < len(nums); i++ {
+		j := i
+		for j > 0 && nums[j] < nums[j-1] {
+			nums[j], nums[j-1] = nums[j-1], nums[j]
+			j--
+		}
+	}
+
+	return nums
+}
+
+func main() {
+	nums := []int{6, 5, 3, 1, 8, 7, 2, 4}
+	fmt.Println(insertionSort(nums)) // [1 2 3 4 5 6 7 8]
+
+}
