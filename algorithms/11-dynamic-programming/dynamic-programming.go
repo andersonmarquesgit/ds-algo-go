a0cf786fa0cce0d09f1ebfddc5339ad84e87af9f
Anderson Marques <anderson.marques-ext@ab-inbev.com>
2024-12-26 23:19:53 -0300

    Add dynamic programming

--- algorithms/11-dynamic-programming/dynamic-programming.go
+++ algorithms/11-dynamic-programming/dynamic-programming.go
@@ -10,12 +10,30 @@ func addTo80(n int) int {
 var cache = map[int]int{}
 
 func memoizedAddTo80(n int) int {
+
+	// Se o valor já foi calculado, retornamos o valor do cache
 	if _, ok := cache[n]; ok {
 		return cache[n]
 	} else {
+		// Se não, calculamos o valor e armazenamos no cache
 		fmt.Println("Executo uma vez o cálculo!")
 		cache[n] = n + 80
 		return cache[n]
+	}
+}
+
+// Outra forma de fazer sem usar variável global
+func memoizedAddTo80V2() func(int) int {
+	cache := map[int]int{}
+
+	return func(n int) int {
+		if _, ok := cache[n]; ok {
+			return cache[n]
+		} else {
+			fmt.Println("Executo uma vez o cálculo!")
+			cache[n] = n + 80
+			return cache[n]
+		}
 	}
 }
 
@@ -28,5 +46,11 @@ func main() {
 	fmt.Println(memoizedAddTo80(5))
 	fmt.Println(memoizedAddTo80(5))
 	fmt.Println(memoizedAddTo80(5))
+
+	fmt.Println("")
+	memoizedAddTo80V2 := memoizedAddTo80V2()
+	fmt.Println(memoizedAddTo80V2(5))
+	fmt.Println(memoizedAddTo80V2(5))
+	fmt.Println(memoizedAddTo80V2(5))
 
 }
